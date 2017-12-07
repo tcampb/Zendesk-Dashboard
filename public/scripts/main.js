3214690a00eee2d58c3bda9884de2049af6547bb
Tyler Campbell <tcampb30@gmail.com>
2017-12-07 17:34:01 -0500

    Added Ajax to handle GET requests

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,11 @@
 $(document).ready(function(){
-
-    while (true) {
-    setTimeout(function(){
-       
-
-
+    setInterval(function(){
     $.get('/userRecords', function(data){
         console.log(JSON.parse(data));
         $.each(JSON.parse(data), function(key, value){
-            if (key === "Hannah Tatum") {
-                $(`[data-user="${key}"`).text(`${value}`);
-    }
+            $(`[data-user="${key}"`).text(`${value}`);
         });
-
-    });
     
-
-
+    });
     }, 1000);
-}
 });
