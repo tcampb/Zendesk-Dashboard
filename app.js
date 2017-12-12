2343f5bc5e17d731ed93c6eb538a163127e516dd
Tyler Campbell <tcampb30@gmail.com>
2017-12-12 15:42:31 -0500

    Fixed form submit issue

--- app.js
+++ app.js
@@ -22,13 +22,18 @@ const re = new RegExp('..:..:..');
 // }, 1000);
 
 //Load static resources (HTML, CSS, JS)
-
 app.use('/admin', express.static('admin'));
 app.use('/', express.static('public'));
 //Handle GET requests for userRecords
+app.post('/admin', parseURL, function(req, res){
+    //Send admin page if authentication is successful
+    req.body && res.sendFile('/Users/tylercampbell/Desktop/node/admin/authentication/index.html');
+});
+
+
 app.get('/userRecords', function(req, res){
   fs.readFile('userRecordsV2.json', 'utf8', function(err, data){
-    userRecords = JSON.parse(data);
+    data ? userRecords = JSON.parse(data) : userRecords = "";
     res.send(JSON.stringify(userRecords));
     });
 });
@@ -42,24 +47,6 @@ app.get('/dailyGoal', function(req, res){
 
 
 //Handle post requests from Zendesk
-// app.post('/api', parseJSON, function(req, res){
-//   var userObject = req.body;
-
-//   fs.readFile('currentUsers.json', 'utf8', function(err, data){
-//       currentUsers = JSON.parse(data);
-//       fs.readFile('userRecords.json', 'utf8', function(err, data){
-//         //Ensure that user in POST request is listed in the currentUsers object
-//         var userReq = userObject.name;
-//         userRecords = JSON.parse(data);
-//         Object.values(currentUsers).includes(userObject.name) ? userRecords[userReq] += 1 : console.log("User not found");
-//         fs.writeFile('userRecords.json', JSON.stringify(userRecords));
-//         res.status(201).send("Success");
-//      });
-//   }); 
-  
-// })
-
-//v2
 app.post('/post', parseURL, function(req, res){
   fs.writeFile('dailyGoal.json', JSON.stringify(req.body));
   res.redirect('/admin');
@@ -68,7 +55,9 @@ app.post('/post', parseURL, function(req, res){
 
 app.post('/delete', parseURL, function(req, res){
   var userName = JSON.parse(JSON.stringify(req.body));
-  userName = userName.Username;
+  var userFound;
+  userName = userName.username;
+  console.log(userName);
   function findUser(object, name){
     for (key in object) {
       object[key].name === name ? delete object[key] : console.log("Incorrect user");
@@ -79,48 +68,44 @@ app.post('/delete', parseURL, function(req, res){
       var userRecordsObject = JSON.parse(data);
       delete userRecordsObject.solved[userName];
       delete userRecordsObject.assigned[userName];
-      console.log(userRecordsObject);
-      fs.writeFile('userRecordsV2.json', JSON.stringify(userRecordsObject))
+      fs.writeFile('userRecordsV2.json', JSON.stringify(userRecordsObject));
+      res.redirect('/admin');
     });
   }
 
   fs.readFile('currentUsersV2.json', 'utf8', function(err, data){
     
     data ? currentUsers = JSON.parse(data) : currentUsers = {};
-    // currentUsers = JSON.parse(data);
-    // currentUsers = JSON.parse(data);
     currentUserNames = [];
     Object.values(currentUsers).forEach(function(object){
       currentUserNames.push(object.name);
     });
-    // Object.values(currentUsers).includes(userName) ? findUser(currentUsers, userName) : console.log("User not found");
-    currentUserNames.includes(userName) ? findUser(currentUsers, userName) : console.log("User not found");
-    }); 
-
-  res.redirect('/admin');
+    currentUserNames.includes(userName) ? findUser(currentUsers, userName) : res.redirect('/admin?response=error404');
+    });
 });
 
 app.post('/add', parseURL, function(req, res){
     var userInfo = JSON.parse(JSON.stringify(req.body));
     var userName = userInfo.username;
     var imgSrc = userInfo.img;
+    console.log(req.body);
 
     fs.readFile('currentUsersV2.json', 'utf8', function(err, data){
-      // currentUsers = JSON.parse(data);
+      err && res.redirect('/admin/error=500');
       data ? currentUsers = JSON.parse(data) : currentUsers = {};
       var newUserId;
       var userIdArray = Object.keys(currentUsers);
       userIdArray.length != 0 ? newUserId = Number(userIdArray.pop()) + 1 : newUserId = 1;
-      // var newUserId = Number(userIdArray.pop()) + 1;
       currentUsers[newUserId] = {"name": `${userName}`, "imgSrc": `${imgSrc}`};
       fs.writeFile('currentUsersV2.json', JSON.stringify(currentUsers));
       fs.readFile('userRecordsV2.json', 'utf8', function(err, data){
+        err && res.redirect('/admin/error=500');
         data ? userRecordObject = JSON.parse(data) : userRecordObject = {"solved": {}, "assigned": {}};
-        // userRecordObject = JSON.parse(data);
         userRecordObject.solved[userName] = 0;
         userRecordObject.assigned[userName] = 0;
         fs.writeFile('userRecordsV2.json', JSON.stringify(userRecordObject));
-        res.redirect('/admin');
+        // res.redirect('/admin');
+        res.send("test");
         });
       });
 });
@@ -141,10 +126,8 @@ app.post('/api', parseJSON, function(req, res){
         //Ensure that user in POST request is listed in the currentUsers object
         var userAction = Object.keys(userObject)[0];
         var userName = Object.values(userObject)[0];
-        // var userReq = userObject.name;
         userRecords = JSON.parse(data);
         currentUserNames.includes(userName) ? userRecords[userAction][userName] += 1 : console.log("User not found");
-        // Object.values(currentUsers).includes(userName) ? userRecords[userAction][userName] += 1 : console.log("User not found");
         fs.writeFile('userRecordsV2.json', JSON.stringify(userRecords));
         res.status(201).send("Success");
      });
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,31 @@
+function formatAMPM(currentTime){
+    var meridiem;
+    var minutes = currentTime.getMinutes();
+    var hours = currentTime.getHours()
+    currentTime.getHours() >= 12 ? meridiem = "pm" : meridiem = "am";
+    if (minutes < 10) {minutes = `0${minutes}`};
+    if (hours > 12) {hours = hours - 12};
+    if (hours === 0) {hours = 12};
+    var formattedTime = `${hours}:${minutes} ${meridiem}`;
+    return formattedTime;
+}
+
 $(document).ready(function(){
     setInterval(function(){
-    // $.get('/userRecords', function(data){
-    //     console.log(JSON.parse(data));
-    //     $.each(JSON.parse(data), function(key, value){
-    //         $(`[data-user="${key}"`).text(`${value}`);
-
-    //     });
+    var ticketsSolved = 0;
+    var ticketsAssigned = 0;
 
     $.get('/userRecords', function(data){
         $.each(JSON.parse(data), function(userAction, userObject){
             $.each(userObject, function(userName, value){
+                userAction === "solved" ? ticketsSolved += value : ticketsAssigned += value;
                 if (userName === "Hannah Tatum") {
                 var maxValue = $(`[data-progress-${userAction}="${userName}"]`).attr('aria-valuemax');
                 $(`[data-progress-${userAction}="${userName}"`).attr({
                                                                   "aria-valuenow": `${value}`,
                                                                   "style": `width: ${(value / maxValue) * 100}%`
                                                                  });
-                    
+                $(`[data-span-${userAction}="${userName}"`).text(`${value}`);
                                                                 }
 
             });
@@ -24,8 +33,21 @@ $(document).ready(function(){
     });
     $.get('/dailyGoal', function(data){
         var data = JSON.parse(data);
-        $('.footer').text(`Daily Goal: ${data.ticketNumber} ${data.ticketType} ticket's by ${data.time}`);
-
+        var goalHour = data.time.slice(0, 2);
+        var goalMinutes = data.time.slice(3);
+        var goalTime = new Date();
+        goalTime.setHours(goalHour);
+        goalTime.setMinutes(goalMinutes);
+        var currentTime = new Date();
+        var currentTimeAMPM = formatAMPM(currentTime);
+        $('.time').text(currentTime.toDateString() + " " + currentTimeAMPM);
+        $('.footer').text(`Daily Goal: ${data.ticketNumber} ${data.ticketType} ticket's by ${formatAMPM(goalTime)}`);
+        if (data.ticketType === "solved" && ticketsSolved >= data.ticketNumber && currentTime <= goalTime) {
+            // CSS animations
+        } else if (data.ticketType === "assigned" && ticketsAssigned >= data.ticketNumber && currentTime <= goalTime) {
+            // CSS animations
+        }
     })
+
     }, 1000);
 });
