32d1276afb7c0b7efb9e1c0be571851d9d5b07ec
Tyler Campbell <tcampb30@gmail.com>
2017-12-12 16:11:19 -0500

    Added authentication

---
+++ admin/scripts/auth.js
@@ -0,0 +1,51 @@
+function configureData(){
+   
+    // Initialize Firebase
+    const config = {
+      apiKey: "AIzaSyCamwmj4QqCWe9rykcuqQrzYYlAgkGx8yc",
+      authDomain: "zendesk-dashboard-f13ba.firebaseapp.com",
+      databaseURL: "https://zendesk-dashboard-f13ba.firebaseio.com",
+      projectId: "zendesk-dashboard-f13ba",
+      storageBucket: "zendesk-dashboard-f13ba.appspot.com",
+      messagingSenderId: "787748198564"
+    };
+    firebase.initializeApp(config);
+    const email = $('[data-email]');
+    const password = $('[data-password');
+    const button = $('[data-submit]');
+
+
+
+//Add sign-up event
+
+button.on('click', function(event){
+    event.preventDefault();
+    const emailValue = email.val();
+    const passwordValue = password.val();
+    const auth = firebase.auth();
+
+    promise = auth.signInWithEmailAndPassword(emailValue, passwordValue);
+    promise.catch(function(error){
+        console.log(error.message);
+    });
+    
+});
+
+firebase.auth().onAuthStateChanged(function(user){
+    if (user){
+        $.ajax({
+            type: 'POST',
+            url: '/admin',
+            data: user.uid,
+            success(response){;
+                document.open();
+                document.write(response);
+                document.close();
+            } 
+        })
+    }
+});
+
+
+};
+configureData();
