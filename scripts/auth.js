90927a2f416c2a1049c77e9c3e18fb573023e3fa
Rich Oswald <richoswald72@gmail.com>
2017-12-19 16:55:19 -0500

    cleaned up auth.js formatting

--- scripts/auth.js
+++ scripts/auth.js
@@ -2,12 +2,12 @@ function configureData(){
    
     // Initialize Firebase
     const config = {
-      apiKey: "AIzaSyCamwmj4QqCWe9rykcuqQrzYYlAgkGx8yc",
-      authDomain: "zendesk-dashboard-f13ba.firebaseapp.com",
-      databaseURL: "https://zendesk-dashboard-f13ba.firebaseio.com",
-      projectId: "zendesk-dashboard-f13ba",
-      storageBucket: "zendesk-dashboard-f13ba.appspot.com",
-      messagingSenderId: "787748198564"
+        apiKey: "AIzaSyCamwmj4QqCWe9rykcuqQrzYYlAgkGx8yc",
+        authDomain: "zendesk-dashboard-f13ba.firebaseapp.com",
+        databaseURL: "https://zendesk-dashboard-f13ba.firebaseio.com",
+        projectId: "zendesk-dashboard-f13ba",
+        storageBucket: "zendesk-dashboard-f13ba.appspot.com",
+        messagingSenderId: "787748198564"
     };
     firebase.initializeApp(config);
     const email = $('[data-email]');
@@ -15,14 +15,14 @@ function configureData(){
     const submitButton = $('[data-submit]');
     const passwordReset = $('[data-password-reset]');
 
-//Add sign-in eventListener
+    //Add sign-in eventListener
     submitButton.on('click', function(event){
         event.preventDefault();
         const emailValue = email.val();
         const passwordValue = password.val();
         const auth = firebase.auth();
-
         promise = auth.signInWithEmailAndPassword(emailValue, passwordValue);
+
         // General input error code reporting function
         var errorFcn = function(errorMessage) {
             var msg = document.getElementById(errorMessage);
@@ -36,74 +36,65 @@ function configureData(){
                 emailForm.value = '';
                 pwdForm.value = '';
                 button.style.marginTop = "70px";
+            });
+        };
 
+        // Alerts for "Invalid email", "User not found", and "Wrong password"
+        promise.catch(function(error){
+            if (error.code.includes("invalid-email")) {
+                errorFcn("email-error");
+            }else if (error.code.includes("user-not-found")) {
+                errorFcn("user-error");
+            }else if (error.code.includes("wrong-password")) {
+                errorFcn("password-error");
+            };
+            console.log(error.message);
+            console.log(error.code);
+        });
     });
-};
-    promise.catch(function(error){
-        if (error.code.includes("invalid-email")) {
-            // alert("Email not formatted correctly");
-            errorFcn("email-error");
-        
-          //auth/user-not-found
-        }else if (error.code.includes("user-not-found")) {
-            errorFcn("user-error");
-
-        
-    //auth/wrong-password -> password reset email
-        }else if (error.code.includes("wrong-password")) {
-            errorFcn("password-error");
-    };
-
-    console.log(error.message);
-    console.log(error.code);
-
-});
-});
     
-//Add password reset eventListener
-passwordReset.on('click', function(event){
-    event.preventDefault();
-    const emailValue = email.val();
-    const passwordValue = password.val();
-    const auth = firebase.auth();
-    promise = auth.signInWithEmailAndPassword(emailValue, passwordValue);
-    promise.catch(function(error){
-        console.log(error.code);
-        switch (error.code) {
-            case "////auth/invalid-email":
-                break;
-            case "///////auth/user-not-found":
-                break;
-            default:
-                auth.sendPasswordResetEmail(emailValue).then(function(){
-                    $('.reset').text(`Please check your ${email.val()} inbox for a reset password link.`);
-                }).catch(function(error){
-                    $('.reset').text('An unexpected error occurred. Please try again later.').addClass('error');
-                })
+    //Add password reset eventListener
+    passwordReset.on('click', function(event){
+        event.preventDefault();
+        const emailValue = email.val();
+        const passwordValue = password.val();
+        const auth = firebase.auth();
+        promise = auth.signInWithEmailAndPassword(emailValue, passwordValue);
+        promise.catch(function(error){
+            console.log(error.code);
+            switch (error.code) {
+                case "////auth/invalid-email":
+                    break;
+                case "///////auth/user-not-found":
+                    break;
+                default:
+                    auth.sendPasswordResetEmail(emailValue).then(function(){
+                        $('.reset').text(`Please check your ${email.val()} inbox for a reset password link.`);
+                    }).catch(function(error){
+                        $('.reset').text('An unexpected error occurred. Please try again later.').addClass('error');
+                    })
             }
         });
     });
     
-
-//Return Admin console if authentication is successful
-firebase.auth().onAuthStateChanged(function(user){
-    if (user){
-        localStorage.setItem("userId", user.uid);
-        email.val("");
-        password.val("");
-        $.ajax({
-            type: 'POST',
-            url: '/admin',
-            data: user.uid,
-            success(response){;
-                document.open();
-                document.write(response);
-                document.close();
-            } 
-        })
-    }
-});
-
-
+    //Return Admin console if authentication is successful
+    firebase.auth().onAuthStateChanged(function(user){
+        if (user){
+            localStorage.setItem("userId", user.uid);
+            email.val("");
+            password.val("");
+            $.ajax({
+                type: 'POST',
+                url: '/admin',
+                data: user.uid,
+                success(response){;
+                    document.open();
+                    document.write(response);
+                    document.close();
+                } 
+            })
+        }
+    });
 };
+
 configureData();
